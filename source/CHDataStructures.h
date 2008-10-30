5384a79b30fd2c796182bb3b0b89cc80c8ab8237
Quinn Taylor <quinntaylor@mac.com>
2008-10-30 05:22:17 +0000

    Slight updates to ReadMe and the Doxygen overview page; added ending newline.
    git-svn-id: http://dysart.cs.byu.edu/chsvn/trunk@175 a75b6edf-6070-4da8-89c8-9992cc802e51

--- source/CHDataStructures.h
+++ source/CHDataStructures.h
@@ -54,20 +54,21 @@
 /**
  @mainpage Overview
  
- This framework provides Objective-C protocols for and implementations of common
- data structures which are currently beyond the purview of Apple's extensive and
- flexible <a href="http://developer.apple.com/cocoa/">Cocoa frameworks</a>. The
- collections which are a part of Cocoa (NSArray, NSDictionary, NSSet, etc.) are
- highly optimized and amenable to many situations. Even so, sometimes the use of
- an honest-to-goodness stack, queue, linked list, tree, etc. can greatly improve
- the clarity and comprehensibility of code.
+ <strong>CHDataStructures.framework</strong> is an attempt to create a library
+ of standard data structures which can be used in any Objective-C program, for
+ educational purposes or as a foundation for other data structures to build on.
+ Data structures in this framework adopt Objective-C protocols that define the
+ functionality of and API for interacting with any implementation thereof,
+ regardless of its internals.
  
- This framework is an attempt to create a library of standard data structures
- which can be reliably used in any Objective-C program. Data structures in this
- framework adopt Objective-C protocols which define the functionality of and API
- for interacting with any implementation thereof, regardless of its internals.
+ This framework provides Objective-C implementations of common data structures
+ which are currently beyond the purview of Apple's extensive and flexible
+ <a href="http://developer.apple.com/cocoa/">Cocoa frameworks</a>. Collections
+ that are a part of Cocoa are highly optimized and amenable to many situations.
+ However, sometimes an honest-to-goodness stack, queue, linked list, tree, etc.
+ can greatly improve the clarity and comprehensibility of code.
  
- The data structure protocols include:
+ The currently supported abstract data type protocols include:
  - CHDeque
  - CHHeap
  - CHLinkedList
@@ -75,25 +76,50 @@
  - CHStack
  - CHTree
  
- This framework targets <a href="http://www.apple.com/macosx/">Mac OS X</a>, but
- most of the code could be easily ported to other Objective-C environments, such
- as <a href="http://www.gnustep.org">GNUStep</a>. However, such efforts would be
- better accomplished by forking this project rather than integrating with it,
- for two primary reasons:
+ The code is written for <a href="http://www.apple.com/macosx/">Mac OS X</a>
+ and does use some features of Objective-C 2.0 (part of 10.5 "Leopard"), but
+ most of the code could be easily ported to other Objective-C environments,
+ such as <a href="http://www.gnustep.org">GNUStep</a>. However, such efforts
+ would be better accomplished by forking this project rather than integrating
+ with it, for several main reasons:
  
  <ol>
- <li>Accommodating multiple environments dramatically increases code complexity,
-     and consequently the effort required to test, maintain, and improve it.
- <li>OS X is by far the most prevalent Objective-C environment in use, and
-     bigger, slower library binaries are a disservice to any developer.
+ <li>Supporting multiple environments increases code complexity, and
+ consequently the effort required to test, maintain, and improve it.</li>
+ <li>Libraries that have bigger and slower binaries to accommodate all possible
+ platforms don't help the mainstream developer.</li>
+ <li>Mac OS X is by far the most prevalent Objective-C environment in use, a
+ trend which isn't likely to change soon.</li>
  </ol>
  
- This framework is free software: you can redistribute it and/or modify it under
- the terms of the <a href="http://www.gnu.org/copyleft/lesser.html">GNU Lesser
- General Public License</a>, either version 3 of the License, or (at your option)
- any later version thereof. The source code for the framework is available at
- <a href="http://www.phillipmorelock.com/examples/cocoadata/">this web page</a>.
-  
+ While certain implementations utilize straight C for their internals, this
+ framework is fairly high-level, and uses composition rather than inheritance
+ in most cases. The framework was originally written as an exercise in writing
+ Objective-C code and consisted mainly of ported Java code. In later revisions,
+ performance has gained greater emphasis, but the primary motivation is to
+ provide friendly, intuitive Objective-C interfaces for data structures, not to
+ maximize speed at any cost, which sometimes happens with C++ and the STL. The
+ algorithms should all be sound (i.e., you won't get O(n) performance where it
+ should be O(log n) or O(1), etc.) and perform quite well in general. If your
+ choice of data structure type and implementation are dependent on performance
+ or memory usage, it would be wise to run the benchmarks from Xcode and choose
+ based on the time and memory complexity for specific implementations.
+ 
+ Binaries and source code for the framework is available
+ <a href="http://cocoaheads.byu.edu/code/CHDataStructures/">here</a>. It is
+ organized in an Xcode project with all relevant dependencies, but could also
+ be built by hand if you're masochistic or just like a challenge.
+ 
+ This framework (library) is free software and is licensed to you under the
+ <b>GNU Lesser General Public License (LGPL)</b>, which may be found at
+ <a href="http://www.gnu.org/copyleft/lesser.html">gnu.org</a> or verbatim in
+ the included <a href="LICENSE.html">LICENSE.html</a>. You may redistribute
+ and/or modify this framework under the terms of version 3 of the License, or
+ (at your option) any later version thereof. This basically means that anyone
+ can use this framework for any purpose, but there are important restrictions,
+ so please read and understand the license before using or modifying the code.
+ (The LGPL is commonly interpreted incorrectly, so please be careful.)
+ 
  If you would like to contribute to the library or let me know that you use it,
  please <a href="mailto:quinntaylor@mac.com?subject=CHDataStructures.framework">
  email me</a>. I am very receptive to help, criticism, flames, whatever.
