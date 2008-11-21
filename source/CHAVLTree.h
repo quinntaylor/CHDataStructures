9e6b4a24a6e544f9c382d9a71c3c933e83f80759
Quinn Taylor <quinntaylor@mac.com>
2008-11-21 20:15:17 +0000

    Better documentation for CHAVLTree, with more complete description of the algorithms. Added a diagram of sample AVL tree, and another of problem cases and how to fix them.
    git-svn-id: http://dysart.cs.byu.edu/chsvn/trunk@251 a75b6edf-6070-4da8-89c8-9992cc802e51

--- source/CHAVLTree.h
+++ source/CHAVLTree.h
@@ -37,24 +37,44 @@
  
  AVL trees are more strictly balanced that most self-balancing binary trees, and
  consequently have slower insertion and deletion performance but faster lookup,
- although all operations are still O(log n) in both average and worst cases.
+ although all operations are still O(log n) in both average and worst cases. AVL
+ trees are shallower than their counterparts; for example, the maximum depth of
+ AVL trees is <em>1.44 log n</em>, versus <em>2 log n</em> for Red-Black trees.
+ In practice, AVL trees are quite competitive with other self-balancing trees,
+ and the insertion and removal algorithms are much easier to understand.
  
- In an AVL tree, the heights of the two child subtrees of any node may differ by
- at most one. If one subtree is deeper than the other, one or more rotations are
- required to rebalance the tree. On insertion or deletion, balance is maintained
- by one or more rotations based around the unbalanced node. The performance hit
- for AVL trees exists because AVL algorithms are less tolerant of slight amounts
- of imbalance in the tree, and balances more frequently and more rigorously.
- The upside is that the depth of AVL trees is at most <em>1.44 log n</em>,
- compared to <em>2 log n</em> for Red-Black trees.
+ In an AVL tree, the heights of the child subtrees of any node may differ by at
+ most one. When inserting or deleting, if the absolute value of the difference
+ between subtree depths is ever reaches 2, then one or more rotations around the
+ unbalanced node are required to rebalance the tree. The 4 possible unbalanced
+ cases and how to rebalance them are shown in Figure 1.
+ 
+ <div align="center"><b>Figure 1 - Rebalancing cases in an AVL tree.</b></div>
+ @image html avl-tree-rotations.png
+ 
+ Although traditional AVL algorithms track the height of each node (one plus the
+ maximum of the height of its chilren) this approach requires updating all the
+ heights along the search path when inserting or removing objects. This penalty
+ can be mitigated by instead storing a balance factor for each node, calculated
+ as the height of the right subtree minus the height of the left subtree. If the
+ balance factor of a node is -2 or +2 after modifying the tree, then rebalancing
+ is necessary. Balance factors are updated when rotating, and at each rotation 
+ we must proceed back up the search path, but can drop out of the loop when the
+ balance factor at a node doesn't change.
+ 
+ Figure 2 shows a sample AVL tree, with tree heights in blue and balance factors
+ in red beside each node.
+
+ <div align="center"><b>Figure 2 - Sample AVL tree and balancing data.</b></div>
+ @image html avl-tree-sample.png
 
  AVL trees were originally described in the following paper:
  
  <div style="margin: 0 25px; font-weight: bold;">
 	 G. M. Adelson-Velsky and E. M. Landis.
 	 "An algorithm for the organization of information."
-	 <em>Proceedings of the USSR Academy of Sciences</em>, 146:263–266, 1962.
-	 (English translation in <em>Soviet Mathematics</em>, 3:1259–1263, 1962.)
+	 <em>Proceedings of the USSR Academy of Sciences</em>, 146:263-266, 1962.
+	 (English translation in <em>Soviet Mathematics</em>, 3:1259-1263, 1962.)
  </div>
  */
 @interface CHAVLTree : CHAbstractTree
