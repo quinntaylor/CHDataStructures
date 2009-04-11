a3605ed5fc852c48a42026b65c3a115d1b4c2845
Quinn Taylor <quinntaylor@mac.com>
2009-04-11 15:05:41 +0000

    Minor updates to framework header documentation, including emphasizing the new license.
    git-svn-id: http://dysart.cs.byu.edu/chsvn/trunk@344 a75b6edf-6070-4da8-89c8-9992cc802e51

--- source/CHDataStructures.h
+++ source/CHDataStructures.h
@@ -71,12 +71,9 @@
  The code is written for <a href="http://www.apple.com/macosx/">Mac OS X</a> and does use some features of Objective-C 2.0 (part of 10.5 "Leopard"), but most of the code could be easily ported to other Objective-C environments, such as <a href="http://www.gnustep.org">GNUStep</a>. However, such efforts would be better accomplished by forking this project rather than integrating with it, for several main reasons:
  
  <ol>
- <li>Supporting multiple environments increases code complexity, and
- consequently the effort required to test, maintain, and improve it.</li>
- <li>Libraries that have bigger and slower binaries to accommodate all possible
- platforms don't help the mainstream developer.</li>
- <li>Mac OS X is by far the most prevalent Objective-C environment in use, a
- trend which isn't likely to change soon.</li>
+ <li>Supporting multiple environments increases code complexity, and consequently the effort required to test, maintain, and improve it.</li>
+ <li>Libraries that have bigger and slower binaries to accommodate all possible platforms don't help the mainstream developer.</li>
+ <li>Mac OS X is by far biggest user of Objective-C today, a trend which isn't likely to change soon.</li>
  </ol>
  
  While certain implementations utilize straight C for their internals, this framework is fairly high-level, and uses composition rather than inheritance in most cases. The framework was originally written as an exercise in writing Objective-C code and consisted mainly of ported Java code. In later revisions, performance has gained greater emphasis, but the primary motivation is to provide friendly, intuitive Objective-C interfaces for data structures, not to maximize speed at any cost, which sometimes happens with C++ and the STL. The algorithms should all be sound (i.e., you won't get O(n) performance where it should be O(log n) or O(1), etc.) and perform quite well in general. If your choice of data structure type and implementation are dependent on performance or memory usage, it would be wise to run the benchmarks from Xcode and choose based on the time and memory complexity for specific implementations.
@@ -85,9 +82,7 @@
  
  This framework is is licensed under a variant of the <a href="http://www.isc.org/software/license">ISC license</a>, an extremely simple and permissive free software license approved by the <a href="http://www.fsf.org/licensing/licenses#ISC">Free Software Foundation (FSF)</a> and <a href="http://opensource.org/licenses/isc-license.txt">Open Source Initiative (OSI)</a>. The license for this framework is included in every source file, and is repoduced in its entirety here:
  
- <em>Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.<br>
- <br>
- The software is provided "as is", without warranty of any kind, including all implied warranties of merchantability and fitness. In no event shall the authors or copyright holders be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the software or the use or other dealings in the software.</em>
+ <div style="margin: 0 30px; text-align: justify;"><em>Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.<br><br>The software is provided "as is", without warranty of any kind, including all implied warranties of merchantability and fitness. In no event shall the authors or copyright holders be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the software or the use or other dealings in the software.</em></div>
  
  This license is functionally equivalent to the MIT and two-clause BSD licenses, and replaces the previous use of the <a href="http://www.gnu.org/copyleft/lesser.html">GNU Lesser General Public License (LGPL)</a>, which is much more complex and frequently misunderstood. In addition, using GPL-style licenses (which are generally unfriendly towards commercial software) makes little sense for Mac developers, whether open-source or proprietary.
  
