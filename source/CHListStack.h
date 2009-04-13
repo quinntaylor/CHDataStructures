e0b3d329aeaf5c11e0b47542ed9ba4abafb6da65
Quinn Taylor <quinntaylor@mac.com>
2009-04-13 20:03:17 +0000

    Improved documentation for CHListQueue, CHListStack, and CHMutableArrayStack. Minor formatting tweak to license header comments that appear in all source files.
    git-svn-id: http://dysart.cs.byu.edu/chsvn/trunk@349 a75b6edf-6070-4da8-89c8-9992cc802e51

--- source/CHListStack.h
+++ source/CHListStack.h
@@ -5,7 +5,7 @@
  Copyright (c) 2002, Phillip Morelock <http://www.phillipmorelock.com>
  
  Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
-
+ 
  The software is  provided "as is", without warranty of any kind, including all implied warranties of merchantability and fitness. In no event shall the authors or copyright holders be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the software or the use or other dealings in the software.
  */
 
@@ -19,7 +19,7 @@
  */
 
 /**
- A simple CHStack implemented using a CHSinglyLinkedList.
+ A simple CHStack implemented using a CHSinglyLinkedList. A singly-linked list is a natural choice since objects are only inserted and removed at the top of the stack, which is easily modeled as the head of a linked list. Enumerating from the top of the stack to the bottom also follows the natural ordering of a linked list.
  */
 @interface CHListStack : CHAbstractListCollection <CHStack>
 
--- source/CHMutableArrayStack.h
+++ source/CHMutableArrayStack.h
@@ -5,7 +5,7 @@
  Copyright (c) 2002, Phillip Morelock <http://www.phillipmorelock.com>
  
  Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
-
+ 
  The software is  provided "as is", without warranty of any kind, including all implied warranties of merchantability and fitness. In no event shall the authors or copyright holders be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the software or the use or other dealings in the software.
  */
 
@@ -18,7 +18,11 @@
  */
 
 /**
- A simple CHStack implemented using an NSMutableArray.
+ A simple CHStack implemented using an NSMutableArray. This stack is modeled with the top being the last element in the array, since insertion and removal in an array are much faster at the back than the front. Accordingly, most operations are mirror-image of those in the parent class but just as fast, with the notable exception of NSFastEnumeration.
+ 
+ Rather than passing the call to <code>-countByEnumeratingWithState:objects:count:</code> straight through to the underlying array, we must obtain an NSEnumerator from @c -reverseObjectEnumerator and store it in the fast enumeration @c state struct. Unfortunately for this particular scenario, NSEnumerator objects only return one object per call from the NSFastEnumeration boilerplate, so it's actually slower than using an NSEnumerator directly.
+ 
+ Most users will prefer the performance improvements in CHCircularBufferStack.
  */
 @interface CHMutableArrayStack : CHAbstractMutableArrayCollection <CHStack>
 
