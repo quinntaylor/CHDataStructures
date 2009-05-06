5f89c6bc614ce731c72e1113fc03aa93a54623d4
Quinn Taylor <quinntaylor@mac.com>
2009-05-06 06:56:06 +0000

    Removed extraneous imports of <Foundation/Foundation.h> and fixed a documentation typo.
    git-svn-id: http://dysart.cs.byu.edu/chsvn/trunk@388 a75b6edf-6070-4da8-89c8-9992cc802e51

--- source/CHLinkedList.h
+++ source/CHLinkedList.h
@@ -8,7 +8,6 @@
  The software is  provided "as is", without warranty of any kind, including all implied warranties of merchantability and fitness. In no event shall the authors or copyright holders be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the software or the use or other dealings in the software.
  */
 
-#import <Foundation/Foundation.h>
 #import "Util.h"
 
 /**
